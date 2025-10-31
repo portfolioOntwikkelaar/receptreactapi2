e98400708832991fb4a407c6b1fd9da5b502529b
portfolioOntwikkelaar <ilia.voropaev@hotmail.com>
2025-10-31 13:33:50 +0100

    Build React app for deployment

--- src/Food.js
+++ src/Food.js
@@ -1,81 +1,70 @@
-import axios from 'axios'
-import React, {useState} from 'react'
-import './Food.css'
-import cake from './images/cake2.png'
-import shop from './images/shop.jpeg'
+import axios from 'axios';
+import React, { useState } from 'react';
+import './Food.css';
+import cake from './images/cake2.png';
 
 const Food = () => {
+  const [query, setQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const APP_ID = 'a0cb2036';
+  const APP_KEY = '92264c96f86689d81c3ca796c5b28911';
 
-  const  [query, setQuery] = useState('')
-  const [rcp, setRcp] = useState([])
-  // eslint-disable-next-line no-unused-vars
-  const [list,setList] = useState('alcohol-free')
+  const getRecipes = async () => {
+    if (!query) return;
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=alcohol-free`;
+    const result = await axios.get(url);
+    setRecipes(result.data.hits);
+  };
 
-  const APP_ID = 'a0cb2036'  
-  const APP_KEY = '92264c96f86689d81c3ca796c5b28911'  
-
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&&health=${list}`
-
-  async function getRecipe() {
-    const result = await axios.get(url)
-    setRcp(result.data.hits)
-    console.log(result.data)
-  }
-
-  const submitForm = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    getRecipe();
-  }
+    getRecipes();
+  };
+
   return (
-    <div className='App'>
-
-      <div className="header">
-        <nav className='nav'>
-          <div className="logo">
-            <img src={cake} alt="" className='img-1' />
-          </div>
-          <ul className='ul'>
-            <li><a href="#">Sponsors</a></li>
-            <li><a href="#">Klanten</a></li>
-            <li><a href="#">Maaltijd</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-
-      <form className='form_1' onSubmit={submitForm}>
-        <input type="text" placeholder='schrijf maar benaming van het artikel' value={query} onChange={e => setQuery(e.target.value)} />
-        <button className="button-3" type='submit'>Invoeren</button>
-      </form>
-        </nav>
-      
-
-      <div>
-
-        <div className='text'>
-          <h1><span className='span-h2'>Snelste Levering </span>Ter Wereld</h1>
-          <p>Voel Het <span>Smaak</span></p>
+    <div className="header">
+      <nav className="nav">
+        <div className="logo">
+          <img src={cake} alt="logo" className="img-1" />
         </div>
-        <div className='print'>
+        <ul className="ul">
+          <li><a href="#">Sponsors</a></li>
+          <li><a href="#">Klanten</a></li>
+          <li><a href="#">Maaltijden</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
 
-        {rcp.map((item,index) => {
-          return (
-            <div key={index} className='product'>
-              <img src={item['recipe']['image']} alt=""  />
-              <p><span>Benaming : </span>{item['recipe']['label']}</p>
-              <p><span>Calorie : </span>{item['recipe']['calories'].toFixed(2)}</p>
-              <div className='buttons'>
+        <form className="form_1" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Voer een gerecht in..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="button-3" type="submit">Zoeken</button>
+        </form>
+      </nav>
 
-              <p>{<a className='d-2' href={item['recipe']['url']}>Lezen</a>}</p>
-              <button className='button-2'>Bestelling</button>
-              </div>
-              
-            </div>
-          )
-        })}
+      <div className="text">
+        <h1><span className="span-h2">Snelste levering </span>ter wereld</h1>
+        <p>Voel de <span>smaak</span></p>
       </div>
-        </div>
-        </div>
-    </div>
-  )
-}
 
-export default Food
+      <section className="print">
+        {recipes.map((item, index) => (
+          <div key={index} className="product">
+            <img src={item.recipe.image} alt={item.recipe.label} />
+            <h3>{item.recipe.label}</h3>
+            <p><span>Calorieën:</span> {item.recipe.calories.toFixed(2)}</p>
+            <div className="buttons">
+              <a className="read-btn" href={item.recipe.url} target="_blank" rel="noreferrer">Bekijken</a>
+              <button className="button-2">Bestellen</button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Food;
